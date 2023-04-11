7f9a6b1b2f41ae94135aedfa5b4d589f2295cfd4
SamiraFonseca <127215725+SamiraFonseca@users.noreply.github.com>
2023-04-11 09:30:32 -0300

    Add files via upload

---
+++ pweb28fev/Insumos/LoginServlet.java
@@ -0,0 +1,30 @@
+package br.edu.ifgoiano.controle;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 7036815389008473423L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(req.getParameter("email"));
+		System.out.println(req.getParameter("senha"));
+		
+		String html = new String();
+		html = "<html><body><h1>Login realizado com sucesso!</h1></body></html>";
+				
+		PrintWriter writer = resp.getWriter();
+		writer.println(html);
+	}
+}
+
+
