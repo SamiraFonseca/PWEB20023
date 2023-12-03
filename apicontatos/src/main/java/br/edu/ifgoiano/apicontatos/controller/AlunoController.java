20bc4cd207b0d6e6d8add04168d9c1e21a72c60c
2021108102540065 <2021108102540065@CMPTRI-MEC-06.ifgoiano.local>
2023-12-03 06:34:10 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- apicontatos/src/main/java/br/edu/ifgoiano/apicontatos/controller/AlunoController.java
+++ apicontatos/src/main/java/br/edu/ifgoiano/apicontatos/controller/AlunoController.java
@@ -2,6 +2,7 @@ package br.edu.ifgoiano.apicontatos.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,8 +18,13 @@ public class AlunoController {
 	AlunoRepository alRepo; 
 
 	@PostMapping
-	public String registrarProf(Aluno al) {
+	public String registrarAluno(Aluno al) {
 		alRepo.save(al);
+		return "CadastrarAluno";
+	}
+	
+	@GetMapping ("/cadastrarAluno")
+	public String cadastrarAluno (){
 		return "CadastrarAluno";
 	}
 }
